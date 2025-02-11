f5123a035514955ca362cca894682d6665d57fe6
Yuqi Liang <yuqi.liang.1900@gmail.com>
2025-02-11 16:53:21 -0600

    1. Add the sequenzo folder under the python_source_code folder for a better package structure. 2. Update seqdef.py, __init__.py, and setup.py

---
+++ python_source_code/sequenzo/__init__.py
@@ -0,0 +1,20 @@
+"""
+@Author  : Yuqi Liang 梁彧祺
+@File    : __init__.py.py
+@Time    : 11/02/2025 16:41
+@Desc    : 
+"""
+# Import the core functions that should be directly available from the sequenzo package
+from .seqdef import *
+# from .visualization.sequence_index_plot import sequence_index_plot
+# from .visualization.state_distribution_plot import state_distribution_plot
+# from .clara.seqclararange import seqclararange
+# from .dissimilarities.seqdist import seqdist
+
+# Define `__all__` to specify the public API when using `from sequenzo import *`
+# __all__ = [
+#     "sequence_index_plot",
+#     "state_distribution_plot",
+#     "seqclararange",
+#     "seqdist"
+# ]
---
+++ python_source_code/setup.py
@@ -0,0 +1,14 @@
+"""
+@Author  : Yuqi Liang 梁彧祺
+@File    : setup.py.py
+@Time    : 11/02/2025 16:37
+@Desc    : 
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="sequenzo",
+    version="0.1",
+    packages=find_packages(),  # Automatically searching for the sequenzo package
+    install_requires=[],
+)
