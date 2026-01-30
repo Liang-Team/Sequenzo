a492b6437ab5556164b71d4b42edd4737f1456c4
Xinyi Li <1836724126@qq.com>
2026-01-31 00:19:30 +0800

    1. Fixed the seqeunzo version was incorrect in jupyter notebook. 2. Updated the .ipynb for get_distance_matrix_example_for_handling_outputs_too_large.

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@
 """
 # sequenzo/__init__.py (Top-level)
 
-__version__ = "0.1.30git add ."
+__version__ = "0.1.30"
 
 # Lazy import: public submodules
 from sequenzo import (datasets, data_preprocessing, visualization, clustering,
