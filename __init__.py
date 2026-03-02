0dd24bedd970bcde8ae0d34baf0ae64ff8600008
Xinyi Li <1836724126@qq.com>
2026-03-02 14:19:08 +0800

    Bump version to v0.1.35

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@
 """
 # sequenzo/__init__.py (Top-level)
 
-__version__ = "0.1.34"
+__version__ = "0.1.35"
 
 # Lazy import: public submodules
 from sequenzo import (datasets, data_preprocessing, visualization, clustering,
